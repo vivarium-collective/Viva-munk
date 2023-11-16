54e6533cbe7b3a49ae8f6c3ee216ab44bd4028c9
Eran <agmon.eran@gmail.com>
2023-11-16 09:32:16 -0500

    get started with multibody process

---
+++ pymunk_process/multibody.py
@@ -0,0 +1,311 @@
+"""
+=========================
+Multibody physics process
+=========================
+
+Simulates collisions between cell bodies with a physics engine.
+"""
+
+import random
+import math
+import copy
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+# vivarium imports
+from pymunk_process.pymunk_minimal import PymunkMinimal as Pymunk
+from pymunk_process.units import units, remove_units
+from process_bigraph import Process, Composite, process_registry
+
+
+DEFAULT_LENGTH_UNIT = units.um
+DEFAULT_BOUNDS = [200 * DEFAULT_LENGTH_UNIT, 200 * DEFAULT_LENGTH_UNIT]
+DEFAULT_MASS_UNIT = units.ng
+DEFAULT_VELOCITY_UNIT = units.um / units.s
+
+# constants
+PI = math.pi
+
+
+# helper functions
+def daughter_locations(mother_location, state):
+    """Given a mother's location, place daughters close"""
+    mother_diameter = state['diameter']
+    mother_x = mother_location[0]
+    mother_y = mother_location[1]
+    locations = []
+    for daughter in range(2):
+        location = [mother_x + random.gauss(0, 0.1) * mother_diameter,
+                    mother_y + random.gauss(0, 0.1) * mother_diameter]
+        locations.append(location)
+    return locations
+
+
+def sphere_volume_from_diameter(diameter):
+    """Given a diameter for a sphere, return the volume"""
+    radius = diameter / 2
+    volume = 4 / 3 * (PI * radius ** 3)
+    return volume
+
+
+def make_random_position(bounds):
+    """Return a random position with the [x, y] bounds"""
+    return [
+        np.random.uniform(0, bound.magnitude) * bound.units
+        for bound in bounds]
+
+
+def add_to_dict(d, added):
+    for k, v in added.items():
+        if k in d:
+            d[k] += v
+        else:
+            d[k] = v
+    return d
+
+
+def remove_from_dict(d, removed):
+    for k, v in removed.items():
+        if k in d:
+            d[k] -= v
+        else:
+            d[k] = -v
+    return d
+
+
+class Multibody(Process):
+    """Multibody process for tracking cell bodies.
+
+    Simulates collisions between
+     cell bodies with a physics engine.
+
+    :term:`Ports`:
+    * ``agents``: The store containing all cell sub-compartments. Each cell in
+      this store has values for location, diameter, mass.
+
+    Arguments:
+        parameters(dict): Accepts the following configuration keys:
+
+        * **jitter** (float): random force applied to agents, in pN.
+        * **bounds** (list): size of the environment in the units specified by length_unit, with ``[x, y]``.
+        * **length_unit** (Quantity): standard length unit for the physics engine -- this includes the bounds.
+        * **mass_unit** (Quantity): standard mass unit for the physics engine.
+        * **velocity_unit** (Quantity): standard velocity unit for the physics engine.
+        * ***animate*** (:py:class:`bool`): interactive matplotlib option to
+          animate multibody. To run with animation turned on set True, and use
+          the TKAgg matplotlib backend:
+
+          .. code-block:: console
+
+              $ MPLBACKEND=TKAgg python tumor_tcell/processes/neighbors.py
+    """
+
+    config_schema = {
+        'jitter_force': 1e-6,
+        'bounds': remove_units(DEFAULT_BOUNDS),
+        'length_unit': DEFAULT_LENGTH_UNIT,
+        'mass_unit': DEFAULT_MASS_UNIT,
+        'velocity_unit': DEFAULT_VELOCITY_UNIT,
+        'animate': False,
+    }
+
+    def __init__(self, config=None):
+        super().__init__(config)
+
+        self.length_unit = self.config['length_unit']
+        self.mass_unit = self.config['mass_unit']
+        self.velocity_unit = self.config['velocity_unit']
+        self.neighbor_distance = self.config['neighbor_distance'].to(self.length_unit).magnitude
+        self.cell_loc_units = {}
+
+        # make the multibody object
+        timestep = self.config['timestep']
+        pymunk_config = {
+            'cell_shape': 'circle',
+            'jitter_force': self.config['jitter_force'],
+            'bounds': [
+                b.to(self.length_unit).magnitude
+                for b in config['bounds']],
+            'physics_dt': min(timestep / 10, 0.1)}
+        self.physics = Pymunk(pymunk_config)
+
+        # interactive plot for visualization
+        self.animate = self.config['animate']
+        if self.animate:
+            plt.ion()
+            self.ax = plt.gca()
+            self.ax.set_aspect('equal')
+
+    def ports_schema(self):
+        glob_schema = {
+            '*': {
+                'boundary': {
+                    'location': {
+                        '_emit': True,
+                        '_default': [
+                            0.5 * bound for bound in self.config['bounds']],
+                        '_updater': 'set',
+                        '_divider': {
+                            'divider': daughter_locations,
+                            'topology': {
+                                'diameter': ('..', 'diameter',)
+                            },
+                        }
+                    },
+                    'diameter': {
+                        '_emit': True,
+                        '_default': 1.0 * self.length_unit},
+                    'mass': {
+                        '_default': 1.0 * self.mass_unit},
+                    'velocity': {
+                        '_default': 0.0 * self.velocity_unit,
+                    }
+                }
+            }
+        }
+        return {'agents': glob_schema}
+
+    def update(self, state, interval):
+        agents = state['agents']
+
+        # animate before update
+        if self.animate:
+            self.animate_frame(agents)
+
+        # update physics with new agents
+        agents = self.bodies_remove_units(agents)
+        self.physics.update_bodies(agents)
+
+        # run simulation
+        self.physics.run(interval)
+
+        # get new cell positions and neighbors
+        cell_positions = self.physics.get_body_positions()
+
+        # add units to cell_positions
+        cell_positions = self.location_add_units(cell_positions)
+
+        return {
+            'agents': {
+                cell_id: {
+                    'boundary': {
+                        'location': list(cell_positions[cell_id])},
+                } for cell_id in agents.keys()
+            }
+        }
+
+    def bodies_remove_units(self, bodies):
+        """Convert units to the standard, and then remove them
+
+        This is required for interfacing the physics engine, which does not track units
+        """
+        for bodies_id, specs in bodies.items():
+            # convert location
+            bodies[bodies_id]['boundary']['location'] = [loc.to(self.length_unit).magnitude for loc in
+                                                         specs['boundary']['location']]
+            # convert diameter
+            bodies[bodies_id]['boundary']['diameter'] = specs['boundary']['diameter'].to(self.length_unit).magnitude
+            # convert mass
+            bodies[bodies_id]['boundary']['mass'] = specs['boundary']['mass'].to(self.mass_unit).magnitude
+            # convert velocity
+            bodies[bodies_id]['boundary']['velocity'] = specs['boundary']['velocity'].to(self.velocity_unit).magnitude
+        return bodies
+
+    def location_add_units(self, bodies):
+        for body_id, location in bodies.items():
+            bodies[body_id] = [(loc * self.length_unit) for loc in location]
+        return bodies
+
+    def remove_length_units(self, value):
+        return value.to(self.length_unit).magnitude
+
+    def animate_frame(self, agents):
+        """matplotlib interactive plot"""
+        plt.cla()
+        bounds = copy.deepcopy(self.config['bounds'])
+        for cell_id, data in agents.items():
+            # location, orientation, length
+            data = data['boundary']
+            x_center = self.remove_length_units(data['location'][0])
+            y_center = self.remove_length_units(data['location'][1])
+            diameter = self.remove_length_units(data['diameter'])
+
+            # get bottom left position
+            radius = (diameter / 2)
+            x = x_center - radius
+            y = y_center - radius
+
+            # Create a circle
+            circle = patches.Circle((x, y), radius, linewidth=1, edgecolor='b')
+            self.ax.add_patch(circle)
+
+        xl = self.remove_length_units(bounds[0])
+        yl = self.remove_length_units(bounds[1])
+        plt.xlim([-xl, 2 * xl])
+        plt.ylim([-yl, 2 * yl])
+        plt.draw()
+        plt.pause(0.01)
+
+
+process_registry.register('multibody', Multibody)
+
+
+def get_agent_config(
+        location=None,
+        bounds=None,
+        velocity=None,
+        # volume=None,
+        diameter=None,
+        mass=None,
+):
+    diameter = diameter or 5 * DEFAULT_LENGTH_UNIT
+    mass = mass or 5 * DEFAULT_MASS_UNIT
+    volume = sphere_volume_from_diameter(diameter)
+
+    bounds = bounds or DEFAULT_BOUNDS
+    if location:
+        location = [loc * bounds[n] for n, loc in enumerate(location)]
+    else:
+        location = make_random_position(bounds)
+
+    return {
+        'boundary': {
+            'location': location,
+            'velocity': velocity,
+            'volume': volume,
+            'diameter': diameter,
+            'mass': mass,
+            'thrust': 0,
+            'torque': 0,
+        }
+    }
+
+def test_multibody():
+    bounds = DEFAULT_BOUNDS
+    state = {
+        'multibody': {
+            '_type': 'process',
+            'address': 'local:multibody',
+            'config': {
+                'bounds': bounds
+            },
+            'wires': {
+                'agents': 'agents'
+            }
+        },
+        'agents': {
+            '0': get_agent_config(bounds=bounds)
+        }
+    }
+
+    sim = Composite({
+        'state': state
+    })
+
+
+
+if __name__ == '__main__':
+    test_multibody()
---
+++ pymunk_process/pymunk_minimal.py
@@ -0,0 +1,311 @@
+import random
+import math
+
+import pymunk
+
+
+PI = math.pi
+
+
+def random_body_position(body):
+    ''' pick a random point along the boundary'''
+    diameter = body.diameter
+    if random.randint(0, 1) == 0:
+        # force along ends
+        if random.randint(0, 1) == 0:
+            # force on the left end
+            location = (random.uniform(0, diameter), 0)
+        else:
+            # force on the right end
+            location = (random.uniform(0, diameter), diameter)
+    else:
+        # force along length
+        if random.randint(0, 1) == 0:
+            # force on the bottom end
+            location = (0, random.uniform(0, diameter))
+        else:
+            # force on the top end
+            location = (diameter, random.uniform(0, diameter))
+    return location
+
+def random_direction(velocity):
+    angle = random.uniform(0, 2*PI)
+    return (velocity * math.cos(angle), velocity * math.sin(angle))
+
+
+class NullScreen(object):
+    def update_screen(self):
+        pass
+    def configure(self, config):
+        pass
+
+
+class PymunkMinimal(object):
+    """
+    Multibody object for interfacing with pymunk
+    """
+
+    defaults = {
+        'cell_shape': 'segment',
+        # hardcoded parameters
+        'elasticity': 0.9,
+        'friction': 0.9,
+        # configured parameters
+        'jitter_force': 0.0,
+        'physics_dt': 0.01,
+        'bounds': [20, 20],
+        'barriers': False,
+        'initial_cells': {},
+    }
+
+    def __init__(self, config):
+        # hardcoded parameters
+        self.elasticity = self.defaults['elasticity']
+        self.friction = self.defaults['friction']
+
+        # configured parameters
+        self.physics_dt = config.get('physics_dt', self.defaults['physics_dt'])
+        self.cell_shape = config.get('cell_shape', self.defaults['cell_shape'])
+        self.bounds = config.get('bounds', self.defaults['bounds'])
+        self.jitter_force = config.get('jitter_force', self.defaults['jitter_force'])
+
+        # initialize pymunk space
+        self.space = pymunk.Space()
+
+        # add static barriers
+        barriers = config.get('barriers', self.defaults['barriers'])
+        self.add_barriers(self.bounds, barriers)
+
+        # initialize cells
+        initial_cells = config.get('initial_cells', self.defaults['initial_cells'])
+        self.bodies = {}
+        for cell_id, specs in initial_cells.items():
+            self.add_body_from_center(cell_id, specs)
+
+    def run(self, timestep):
+        if self.physics_dt > timestep:
+            print('timestep skipped by pymunk_multibody: {}'.format(timestep))
+            return
+
+        time = 0
+        while time < timestep:
+            time += self.physics_dt
+
+            # apply forces
+            if self.jitter_force > 0:
+                for body in self.space.bodies:
+                    self.apply_jitter_force(body)
+
+            # run for a physics timestep
+            self.space.step(self.physics_dt)
+
+    def apply_jitter_force(self, body):
+        jitter_location = random_body_position(body)
+        jitter_force = [
+            random.normalvariate(0, self.jitter_force),
+            random.normalvariate(0, self.jitter_force)]
+        body.apply_impulse_at_local_point(
+            jitter_force,
+            jitter_location)
+
+    def add_barriers(self, bounds, barriers):
+        """ Create static barriers """
+        thickness = 100.0
+        offset = thickness
+        x_bound = bounds[0]
+        y_bound = bounds[1]
+
+        static_body = self.space.static_body
+        static_lines = [
+            pymunk.Segment(
+                static_body,
+                (0.0-offset, 0.0-offset),
+                (x_bound+offset, 0.0-offset),
+                thickness),
+            pymunk.Segment(
+                static_body,
+                (x_bound+offset, 0.0-offset),
+                (x_bound+offset, y_bound+offset),
+                thickness),
+            pymunk.Segment(
+                static_body,
+                (x_bound+offset, y_bound+offset),
+                (0.0-offset, y_bound+offset),
+                thickness),
+            pymunk.Segment(
+                static_body,
+                (0.0-offset, y_bound+offset),
+                (0.0-offset, 0.0-offset),
+                thickness),
+        ]
+
+        if barriers:
+            spacer_thickness = barriers.get('spacer_thickness', 0.1)
+            channel_height = barriers.get('channel_height')
+            channel_space = barriers.get('channel_space')
+            n_lines = math.floor(x_bound/channel_space)
+
+            machine_lines = [
+                pymunk.Segment(
+                    static_body,
+                    (channel_space * line, 0),
+                    (channel_space * line, channel_height), spacer_thickness)
+                for line in range(n_lines)]
+            static_lines += machine_lines
+
+        for line in static_lines:
+            line.elasticity = 0.0  # bounce
+            line.friction = 0.8
+            self.space.add(line)
+
+    def get_shape(self, boundary):
+        '''
+        shape documentation at: https://pymunk-tutorial.readthedocs.io/en/latest/shape/shape.html
+        '''
+
+        diameter = boundary['diameter']
+        half_diameter = diameter / 2
+        shape = pymunk.Circle(None, radius=half_diameter, offset=(0, 0))
+
+        return shape
+
+    def get_inertia(self, shape, mass):
+        radius = shape.radius
+        inertia = pymunk.moment_for_circle(mass, radius, radius)
+        return inertia
+
+    def add_body_from_center(self, body_id, specs):
+        boundary = specs['boundary']
+        mass = boundary['mass']
+        center_position = boundary['location']
+        diameter = boundary['diameter']
+
+        # get shape, inertia, make body, assign body to shape
+        shape = self.get_shape(boundary)
+        inertia = self.get_inertia(shape, mass)
+        body = pymunk.Body(mass, inertia)
+        shape.body = body
+
+        body.position = (
+            center_position[0],
+            center_position[1])
+        body.diameter = diameter
+
+        shape.elasticity = self.elasticity
+        shape.friction = self.friction
+
+        # add body and shape to space
+        self.space.add(body, shape)
+
+        # add body to cells dictionary
+        self.bodies[body_id] = (body, shape)
+
+    def set_velocity(self, body_id, velocity):
+        body, shape = self.bodies[body_id]
+        body.velocity = random_direction(velocity)
+
+    def update_body(self, body_id, specs):
+        boundary = specs['boundary']
+        diameter = boundary['diameter']
+        mass = boundary['mass']
+
+        body, shape = self.bodies[body_id]
+        position = body.position
+
+        # get shape, inertia, make body, assign body to shape
+        new_shape = self.get_shape(boundary)
+        inertia = self.get_inertia(new_shape, mass)
+        new_body = pymunk.Body(mass, inertia)
+        new_shape.body = new_body
+
+        new_body.position = position
+        new_body.velocity = body.velocity
+        new_body.diameter = diameter
+
+        new_shape.elasticity = shape.elasticity
+        new_shape.friction = shape.friction
+
+        # swap bodies
+        self.space.remove(body, shape)
+        self.space.add(new_body, new_shape)
+
+        # update body
+        self.bodies[body_id] = (new_body, new_shape)
+
+        if 'velocity' in boundary:
+            self.set_velocity(body_id, boundary['velocity'])
+
+    def update_bodies(self, bodies):
+        # if an cell has been removed from the cells store,
+        # remove it from space and bodies
+
+        removed_bodies = [
+            body_id for body_id in self.bodies.keys()
+            if body_id not in bodies.keys()]
+        for body_id in removed_bodies:
+            body, shape = self.bodies[body_id]
+            self.space.remove(body, shape)
+            del self.bodies[body_id]
+
+        # update cells, add new cells
+        for body_id, specs in bodies.items():
+            if body_id in self.bodies:
+                self.update_body(body_id, specs)
+            else:
+                self.add_body_from_center(body_id, specs)
+
+    def get_body_position(self, cell_id):
+        body, shape = self.bodies[cell_id]
+        return tuple(pos for pos in body.position)
+
+    def get_body_positions(self):
+        return {
+            body_id: self.get_body_position(body_id)
+            for body_id in self.bodies.keys()}
+
+
+
+def test_minimal(
+        total_time=2,
+        cell_shape='rectangle',
+        velocity=10.0,  #  um / min
+        n_cells=1,
+):
+
+    bounds = [500, 500]
+    center_location = [0.5*loc for loc in bounds]
+    cell_ids = [str(cell_idx) for cell_idx in range(n_cells)]
+    cells = {
+        cell_id: {
+            'boundary': {
+                'location': center_location,
+                'volume': 15,
+                'diameter': 30,
+                'mass': 1,
+                'velocity': velocity,
+            }}
+        for cell_id in cell_ids
+    }
+    config = {
+        'cell_shape': cell_shape,
+        'bounds': bounds,
+        'barriers': False,
+        'initial_cells': cells,
+    }
+    multibody = PymunkMinimal(config)
+
+    # run simulation
+    time = 0
+    time_step = 1
+    while time < total_time:
+        time += time_step
+        multibody.run(time_step)
+        for cell_id in cell_ids:
+            multibody.set_velocity(cell_id, velocity)
+
+        print(multibody.get_body_positions())
+
+
+if __name__ == '__main__':
+    test_minimal(
+        total_time=10)
